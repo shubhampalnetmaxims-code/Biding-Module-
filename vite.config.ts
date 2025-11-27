740391631ab0ff4515e198ff8ea35d60fd9ac6d7
shubhampalnetmaxims-code <shubhampal.netmaxims@gmail.com>
2025-11-27 18:25:35 +0530

    Updated index.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      build: {
+        rollupOptions: {
+          input: path.resolve(__dirname, 'index.html'),
+        },
+      },
       server: {
         port: 3000,
         host: '0.0.0.0',
